0951c0733264afcb8cd7503c40f1bfa733001061
Jacob Robinett <robinett258@gmail.com>
2022-03-16 00:16:30 -0500

    changed col spacing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('shop_new.html').pipe(res)
+  fs.createReadStream('shop.html').pipe(res)
 })
 
 server.listen(process.env.PORT || 8000) 
